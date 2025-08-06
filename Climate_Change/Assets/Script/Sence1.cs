0e8353ae8008d1177c289dc7344c8d4394921091
Aura <aura01221@gmail.com>
2025-08-07 02:23:09 +0800

    fix(climate_change): only change scene when press 2

--- Climate_Change/Assets/Script/Sence1.cs
+++ Climate_Change/Assets/Script/Sence1.cs
@@ -6,9 +6,7 @@ public class Sence1 : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1)
-        || Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2)
-        || Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3))
+        if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
         {
             _gameManager.SenceChange(2);
         }
--- Climate_Change/Assets/Script/Sence2.cs
+++ Climate_Change/Assets/Script/Sence2.cs
@@ -21,9 +21,7 @@ public class Sence2 : MonoBehaviour, TimerController
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1)
-        || Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2)
-        || Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3))
+        if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
         {
             _gameManager.SenceChange(3);
         }
