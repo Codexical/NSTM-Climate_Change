23ebd69bacf511e37f52de5c20dadd28fef94062
Aura <aura01221@gmail.com>
2025-08-09 23:59:43 +0800

    feat(climate_change): config for debug

--- Green_House/Assets/Script/GameManager.cs
+++ Green_House/Assets/Script/GameManager.cs
@@ -1,11 +1,36 @@
 using UnityEngine;
 
+
+[System.Serializable]
+public class Config
+{
+    public bool showDebugObjects;
+}
+
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] _sences;
+    [SerializeField] private GameObject[] _debugObjects;
+    string configPath = "./config.json";
 
     private void Start()
     {
+        if (!System.IO.File.Exists(configPath))
+        {
+            // Create default config file with showDebugObjects = false
+            Config defaultConfig = new Config { showDebugObjects = false };
+            string defaultJson = JsonUtility.ToJson(defaultConfig, true);
+            System.IO.File.WriteAllText(configPath, defaultJson);
+        }
+        string json = System.IO.File.ReadAllText(configPath);
+        Config config = JsonUtility.FromJson<Config>(json);
+        if (config != null)
+        {
+            foreach (GameObject debugObject in _debugObjects)
+            {
+                debugObject.SetActive(config.showDebugObjects);
+            }
+        }
         SenceChange(1);
     }
     public void SenceChange(int senceIndex)
