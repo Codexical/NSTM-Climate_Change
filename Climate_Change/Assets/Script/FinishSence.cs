da0e13f3bc49478ec71915fffd3a4612dfff8b1a
Aura <aura01221@gmail.com>
2025-08-21 20:11:17 +0800

    fix(climate_change): remove unused code

--- Climate_Change/Assets/Script/FinishSence.cs
+++ Climate_Change/Assets/Script/FinishSence.cs
@@ -19,14 +19,4 @@ public class FinishSence : MonoBehaviour, TimerController
     {
         _gameManager.SenceChange(1);
     }
-    void Update()
-    {
-        // if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1)
-        // || Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2)
-        // || Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3))
-        // {
-        //     _gameManager.SenceChange(1);
-        // }
-
-    }
 }
